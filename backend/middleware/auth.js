c23ca8bf2e56e8bffebc93305f20d469e1ba53a3
Max <tourniermaxime@orange.fr>
2022-01-15 22:19:11 +0100

    Ajout des variables d'environnement

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   try {
     //Token lié à l'id de l'utilisateur
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
     const userId = decodedToken.userId;
     req.auth = { userId };
     //Si incorrect utilisateur invalide
