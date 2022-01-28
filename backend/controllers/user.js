ac9f5eef1f9bffdc6a5a5bbfa36abcff9bd26e05
Max <tourniermaxime@orange.fr>
2022-01-28 20:51:45 +0100

    Modifiacation du délai d'expiration du token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -55,7 +55,7 @@ exports.login = (req, res, next) => {
           res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
-              expiresIn: "24h",
+              expiresIn: "4h",
             }),
           });
         })
